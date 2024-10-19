734645b858fbc44699ec3d6545e8bc17a6f48c44
CoffeeAbuser67 <Hservers170224@gmail.com>
2024-10-19 00:53:59 -0300

    checkpoint

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,15 +3,14 @@
 import axios from 'axios';
 
 // _PIN_ Nginx proxy : 'http://127.0.0.1:81/api/v1';
-
 // _PIN_ Local Server : 'http://127.0.0.1:8000/api/v1';
 
-const BASE_URL = 'http://127.0.0.1:81/api/v1';
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosDefault = axios.create({
     baseURL: BASE_URL,
 });
-
 
 // NOTE
 // For requests made to the same origin (same domain), 
