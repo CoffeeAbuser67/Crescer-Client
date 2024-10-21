a1d8f1890d166fb5c024edfac131a28d8cc98f01
CoffeeAbuser67 <Hservers170224@gmail.com>
2024-10-21 11:37:44 -0300

    checkpoint

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 // _PIN_ Local Server : 'http://127.0.0.1:8000/api/v1';
 
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = 'http://127.0.0.1:81/api/v1';
 
 export const axiosDefault = axios.create({
     baseURL: BASE_URL,
